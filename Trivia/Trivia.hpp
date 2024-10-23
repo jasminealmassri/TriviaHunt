cfc6b93bd5e5976f8875f53d12cf4743b7c83e50
Jasmine Al Massri <almassri.jasmine@gmail.com>
2024-10-23 11:03:50 -0400

    get valid input first draft

--- Trivia/Trivia.hpp
+++ Trivia/Trivia.hpp
@@ -5,23 +5,19 @@
 
 class MC {
 	std::string question_;
-	//unsigned num_responses_;
 	std::vector<std::string> responses_;
 	unsigned correct_response_;
 public:
 	MC(
 		  std::string question = ""
-		//, unsigned num_responses = 2
 		, std::vector<std::string> responses = {"True", "False"}
 		, unsigned correct_response = 1
 	) 
 		: question_(question)
-		//, num_responses_(num_responses)
 		, responses_(responses)
 		, correct_response_(correct_response) 
 	{}
 
-	//unsigned get_num_r() const { return num_responses_; }
 	unsigned get_correct_r() const { return correct_response_; }
 	std::vector<std::string> get_reponses() const { return responses_; }
 
--- Trivia/main.cpp
+++ Trivia/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "Trivia.hpp"
+#include "Utility.hpp"
 
 using namespace std;
 
@@ -11,7 +12,19 @@ int main() {
 	vector<string> responses = { "True", "False", "Maybe" };
 	questions.push_back(MC("True or false? ", responses, 0));
 
+
 	for(MC question : questions) {
+		char answer;
 		question.display();
+		do {
+			get_valid_input(cin, answer, "Enter your answer: ");
+			answer = toupper(answer);
+			if (answer < 'A' || answer > 'A' + question.get_reponses().size() - 1) {
+				std::cout << "Invalid response. Please try again.\n";
+			}
+
+		} while (answer < 'A' || answer > 'A' + question.get_reponses().size() - 1);
+		// DEBUG
+		std::cout << "You chose " << answer << std::endl;
 	}
 }
